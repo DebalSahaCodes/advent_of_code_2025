0d6227719f791db0856e5d03c9df829e78098aef
Debal Saha <116924730+DebalSahaCodes@users.noreply.github.com>
2025-12-18 23:08:35 -0800

    Update part1.py to solve sample of day 8 part 1

--- day__8/part1.py
+++ day__8/part1.py
@@ -8,7 +8,7 @@ for line in f_lines:
         line=line[:-1]
     n_lines.append(line)
 
-
+n_totalPOS = len(n_lines)
 dict_dist={}
 
 def get_posList_from_line(line):
@@ -77,20 +77,19 @@ def get_new_key():
     else:
         return list(final_dict.keys())[-1] + 1
 
-def is_found_in_v(v,pos):
-    res=0
-    for val in v:
-        if are_same(val,pos):
-            #print("\t\t:found..breaking")
-            res=1
-            break
-    return res
+#def is_found_in_v(v,pos):
+#    res=0
+#    for val in v:
+#        if are_same(val,pos):
+#            #print("\t\t:found..breaking")
+#            res=1
+#            break
+#    return res
 
-def add_in_final_dict(val_p):
+def find_keys_in_final_dict(val_p):
     #done1or2=0
     key_p0=-1
     key_p1=-1
-    circuits_made = 0
     #----------
     for k,vals in final_dict.items():
         for v in vals:
@@ -102,14 +101,27 @@ def add_in_final_dict(val_p):
                 key_p1=k
             if -1!=key_p0 and -1!=key_p1:
                 break
-    #----------
-    #----------
+    return key_p0,key_p1,val_p
+
+def merge_in_final_dict(key_p0, key_p1):
+    new_val0=final_dict[key_p0]
+    for v in final_dict[key_p1]:
+        new_val0.append(v)
+    del final_dict[key_p1]
+
+
+
+def add_in_final_dict(key_p0, key_p1, val_p, c_shortest):
     if -1!=key_p0 and -1==key_p1: #0 exists; only add 1
         final_dict[key_p0].append(val_p[1])
-        print("\tfound existing:",val_p[0],"adding to key:",key_p0,val_p[1],"to inc. len to", len(final_dict[key_p0]))
+        print("\tfound existing:",val_p[0])
+        print("\tadding to key:",key_p0,val_p[1],"to inc. len to", len(final_dict[key_p0]))
+        #c_shortest += 1
     elif -1==key_p0 and -1!=key_p1: #1 exists; only add 0
         final_dict[key_p1].append(val_p[0])
-        print("\tfound existing:",val_p[1],"adding to key:",key_p1,val_p[0],"to inc. len to", len(final_dict[key_p1]))
+        print("\tfound existing:",val_p[1])
+        print("\tadding to key:",key_p1,val_p[0],"to inc. len to", len(final_dict[key_p1]))
+        #c_shortest += 1
     elif -1==key_p0 and -1==key_p1:
         lst=[]
         lst.append(val_p[0])
@@ -118,27 +130,106 @@ def add_in_final_dict(val_p):
         final_dict[nKEy]=lst
         print("\t:adding new to key:",nKEy,val_p[1])
         print("\t:adding new to key:",nKEy,val_p[0])
-        circuits_made += 1
+        #c_shortest += 1
     else:
-        print("\t:already added so skipping")
-        print("\t\t:",val_p[0])
-        print("\t\t:",val_p[1])
-    return circuits_made
+        str_p_p0 = "\t:already added "
+        str_p_p0 +="\n\t: " + str(val_p[0]) + " to key:" + str(key_p0)
+        str_p_p0 +="\n\t: " + str(val_p[1]) + " to key:" + str(key_p1)
+        if key_p0!=key_p1:
+            str_p_p0 += " \n\t: so merging key " + str(key_p1) + " into " + str(key_p0)
+            merge_in_final_dict(key_p0,key_p1)
+            #c_shortest += 1
+        else:
+            str_p_p0 += " \n\t: so skipping"
+        print(str_p_p0)
+    c_shortest += 1
+    return c_shortest
 
 
+def check_if_new_circuit(key_p0, key_p1):
+    is_new_circuit = 0
+    is_add_circuit = 0
+    if -1!=key_p0 and -1==key_p1: #0 exists; only add 1
+        is_new_circuit = 0 # no new circuit
+        is_add_circuit = 1
+    elif -1==key_p0 and -1!=key_p1: #1 exists; only add 0
+        is_new_circuit = 0 # no new circuit
+        is_add_circuit = 1
+    elif -1!=key_p0 and -1!=key_p1:
+        is_new_circuit = 0 # no new circuit 
+        is_add_circuit = 1 ## MERGE or PASS
+    elif -1==key_p0 and -1==key_p1:
+        is_new_circuit = 1 # YES new circuit
+        is_add_circuit = 1
+    else:
+        is_new_circuit = 0 # no new circuit
+        is_add_circuit = 0
+    return is_new_circuit, is_add_circuit
 
-tot_circuits_made=0
+cur_circuits=0
 max_circuits=10
 
-for _,values in dict_dist.items():
-    for vals in values:
-        if tot_circuits_made > max_circuits:
-            break
-        print("processing",vals)
-        circuits_made = add_in_final_dict(vals)
-        tot_circuits_made += circuits_made
-        print("circuits:",tot_circuits_made)
+def count_circuits_made():
+    total_circuits=0
+    print_str=""
+    done_vals=[]
+    for keys,vals in final_dict.items():
+        total_circuits+=1
+        print_str += str(len(vals)) + " + "
+        for v in vals:
+            done_vals.append(v)
+    n_single_c = n_totalPOS - len(done_vals)
+    if n_single_c>0:
+        print_str += str(n_single_c)
+        total_circuits += n_single_c
+    return total_circuits, print_str
 
+#def create_circuits():
+#    for _,values in dict_dist.items():
+#        for vals in values:
+#            print("\nProcessing",vals)
+#            kp0,kp1,boxP = find_keys_in_final_dict(vals)
+#            is_new_k, to_add = check_if_new_circuit(kp0, kp1)
+#            # count total circuits being made till now
+#            # before adding the currently connected boxes
+#            # to the tally of total-connected-circuits
+#            p_str=""
+#            if len(final_dict.keys())>0:
+#                cur_circuits, p_str = count_circuits_made()
+#                print("circuits:",p_str,"total:",cur_circuits)
+#                f_cur_circuits = cur_circuits
+#                if is_new_k:
+#                    f_cur_circuits += 1
+#                if f_cur_circuits < max_circuits:
+#                    return
+#            if to_add:
+#                add_in_final_dict(kp0, kp1, boxP)
+#                if p_str=="":
+#                    cur_circuits, p_str = count_circuits_made()
+#                    print("circuits:",p_str,"total:",cur_circuits)
+
+def create_circuits():
+    n_shortest = 0
+    for _,values in dict_dist.items():
+        for vals in values:
+            print("\nProcessing",vals)
+            kp0,kp1,boxP = find_keys_in_final_dict(vals)
+            is_new_k, to_add = check_if_new_circuit(kp0, kp1)
+            # count total circuits being made till now
+            # before adding the currently connected boxes
+            # to the tally of total-connected-circuits
+            p_str=""
+            if to_add:
+                n_shortest = add_in_final_dict(kp0, kp1, boxP, n_shortest)
+            cur_circuits, p_str = count_circuits_made()
+            print("circuits:",p_str,"total:",cur_circuits)
+            # check if MAX shortest Connections Already Made
+            if len(final_dict.keys()) and n_shortest>=max_circuits:
+                print(max_circuits, "circuits done!!")
+                return
+
+
+create_circuits()
 
 for k,v in final_dict.items():
     print("\nTotal Values in key:",k,len(v))
